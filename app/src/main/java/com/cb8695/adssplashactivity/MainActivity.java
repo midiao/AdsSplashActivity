e20fae62eeb41b228bd2571915a2b2faa02c28cf
陈旸 <wzdxc123@163.com>
2016-09-11 17:30:27 +0800

    init project

---
+++ app/src/main/java/com/cb8695/adssplashactivity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cb8695.adssplashactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
